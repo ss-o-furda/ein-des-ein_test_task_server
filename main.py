e7fe09aeec5a99ea57b0224298284b5aca2c1495
ss-o-furda <orik7800@gmail.com>
2024-04-15 14:32:42 +0300

    Update to deploy to EC2

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
-from fastapi import FastAPI, Request, staticfiles, responses
+from fastapi import FastAPI, staticfiles
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.templating import Jinja2Templates
 
 from utils.constants import GroupName
 from utils.utils import generate_response
@@ -8,9 +7,7 @@ from utils.utils import generate_response
 app = FastAPI()
 
 app.mount("/images", staticfiles.StaticFiles(directory="images"), name="images")
-app.mount("/assets", staticfiles.StaticFiles(directory="assets"), name="static")
 
-templates = Jinja2Templates(directory="dist")
 
 # only for demo purposes, do not use in real production!!!
 origins = ["*"]
@@ -24,16 +21,6 @@ app.add_middleware(
 )
 
 
-@app.get("/favicon.ico")
-async def get_favicon():
-    return responses.FileResponse("./dist/favicon.ico")
-
-
 @app.get("/data/{group}")
 async def get_test_data(group: GroupName, classes: str | None = None):
     return generate_response(group, classes)
-
-
-@app.get("/{rest_of_path:path}")
-async def react_app(req: Request, rest_of_path: str):
-    return templates.TemplateResponse("index.html", {"request": req})
